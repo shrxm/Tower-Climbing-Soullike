0c8bdee4e05a04695714b24bcc58a12c4e3f3b1b
shrxm <102349239+shrxm@users.noreply.github.com>
2024-04-23 21:18:05 +0900

    boss_colider_set

--- Assets/Scripts/Boss/BossPattern.cs
+++ Assets/Scripts/Boss/BossPattern.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:187a1b3a5ded2338b4cf547073c02f6a286d93725f9b55ddf4df11fa0c362618
-size 4652
+oid sha256:8cf9422f19a0e4977b42057b60750179228ba2c823f8e97df11cc0ebba8b9e09
+size 4356
