fa6b19e3c455d75770ae4dc6859dda870890c81c
shrxm <102349239+shrxm@users.noreply.github.com>
2024-04-27 21:54:59 +0900

    Update BossStatus.cs

--- Assets/Scripts/Boss/BossStatus.cs
+++ Assets/Scripts/Boss/BossStatus.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:642105a52e06b55c2efa1a576815e5936c1f54aceca890b18794471f2c9a10ab
-size 509
+oid sha256:77a0876fce2350a3775afe5c8f298aacc8c85ec64d23651e6ad653d760018090
+size 555
