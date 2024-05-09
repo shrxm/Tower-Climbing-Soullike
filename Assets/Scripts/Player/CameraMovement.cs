801e85263cea2f51584ded439c4098562b67ec05
brawlkim <rudgks3374@gmail.com>
2024-05-10 01:23:17 +0900

    kh_8
    공격 구현 및 락온 구현

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38e88d27863100548becbb4d8146659f881fd0eb72fcd66ba401c63273b7efcf
-size 1418
+oid sha256:ecebbfc24ebf61d1858c2ba2d42df80b74e10bc86347a753ac8d16c57712e5b1
+size 2101
---
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8c314bf99c6931ec4bbfee238243f4c9e9ab7300c596c3965789823b623eb6a
+size 774
