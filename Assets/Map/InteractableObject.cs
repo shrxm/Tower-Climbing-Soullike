1711a1f148d3322b3b9c4f48faca46e33e96b725
Baek jaemin <110534217+hanamza@users.noreply.github.com>
2024-05-24 19:12:24 +0900

    gate

---
+++ Assets/Map/InteractableObject.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:25c01065ad9b3b255c2f708a85c5ea023b912fee70fdcba8ec9034fbe4315f7d
+size 236
---
+++ Assets/Map/PlayerInteraction.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:08b30f27ae6d8586024a60197a8c3641ef13c7ee44735ffdd20533fc2bae8ce3
+size 1167
