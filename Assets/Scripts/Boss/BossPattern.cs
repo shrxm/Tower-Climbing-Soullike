b8a5c408688be88cd12aa2911bb0f28bf4422b3b
shrxm <102349239+shrxm@users.noreply.github.com>
2024-05-26 23:22:30 +0900

    boss

--- Assets/Scripts/Boss/BossPattern.cs
+++ Assets/Scripts/Boss/BossPattern.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8bd578bc37493d507b1905dc99ed57038ceff03c2b4adb847238941fb8f3d41
-size 3981
+oid sha256:fd2aa8be1fe6babd972e789709208657ca29de7bba783b6d373a5112f812b5bf
+size 3971
