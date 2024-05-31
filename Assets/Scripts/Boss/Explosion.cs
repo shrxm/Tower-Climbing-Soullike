a42ff4e144ce64c0b63bfecdf31f74084465aeaf
shrxm <102349239+shrxm@users.noreply.github.com>
2024-06-01 02:54:21 +0900

    sucuss

--- Assets/Scripts/Boss/Explosion.cs
+++ Assets/Scripts/Boss/Explosion.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc4d4a7327447656e615fad4de43dcf5f7787dc7b8b91a0d156e01311370e33a
-size 1817
+oid sha256:ad3b76edfb0348a0e700d256e89150679a18f176c42accfe9727b109f39e0394
+size 2012
