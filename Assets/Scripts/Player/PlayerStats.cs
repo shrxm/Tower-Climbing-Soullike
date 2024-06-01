7224e1f02886e7730c3541637051bacb4fe880c4
brawlkim <rudgks3374@gmail.com>
2024-06-01 18:23:33 +0900

    kh_15
    HP 및 Energy 구현

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d252ecfbf2e24444d67be850f2b945a152e4c56510343da8ccacf36e832cfc7
-size 555
+oid sha256:97f61c3de610679ce13ba4f106479057be9a4fd3b351a2d6d4cf1adfd768010f
+size 473
