dda8e540bbe36f054fa24bba138787b908a02822
brawlkim <rudgks3374@gmail.com>
2024-06-03 02:20:48 +0900

    kh_17
    데미지 파트 수정

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15f49d01c4ad3bacb8528386b8641d6979a6a58b68b0b5202bfe592bddb289cf
-size 808
+oid sha256:4c9f0367f7266eb6b3a2535efec19317b77de654eb8880c00046f83f1bbddcf2
+size 1405
