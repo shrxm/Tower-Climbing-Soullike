177e4faf99b7998c93e680b9ae8bd3997185ef3c
21LEEKANGHYUN <127746054+21LEEKANGHYUN@users.noreply.github.com>
2024-06-03 17:16:24 +0900

    hp,energy bar
    hp바와 energy바 마무리입니다.

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e658a682e31217b2601bb33b14c89112f98387b14d53d98bcafad72bd067eee
-size 4036
+oid sha256:3c3377e43e6fead863ed4b144f60926c3999c69b7ab0b56ba54bc560d52fc830
+size 6120
