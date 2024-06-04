1c4e63730545d5f2aef64dd66f4155ea4e452074
21LEEKANGHYUN <127746054+21LEEKANGHYUN@users.noreply.github.com>
2024-06-04 21:39:53 +0900

    robbyUI
    로비 UI및 석상상호작용

--- Assets/Map/StatusObject.cs
+++ Assets/Map/StatusObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf0f23a4c934316ca80fc4ff5178d696c258365aa343ac4fbe87d2615e70a625
-size 234
+oid sha256:0829fa257e2f2f8a60ec46072c54b3f159bbd82ddf9422a2fc3069ada0956ffb
+size 306
