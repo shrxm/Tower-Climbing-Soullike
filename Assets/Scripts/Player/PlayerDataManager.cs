9ecfc44ae9e8819dc120e7d4cc0a0452649417a5
21LEEKANGHYUN <127746054+21LEEKANGHYUN@users.noreply.github.com>
2024-06-04 20:17:12 +0900

    YOUDIED_1
    죽었을때 캔버스 임시저장

--- Assets/Scripts/Player/PlayerDataManager.cs
+++ Assets/Scripts/Player/PlayerDataManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b6f1f7fd1621d1c03cda2bc9ef01c77785e06884804705ac25ee5963cc25aa6
-size 1629
+oid sha256:5826fe821672a7a8c3f836914eae214e5a6ae7465b97d92966a6ada18f8a3d40
+size 1631
