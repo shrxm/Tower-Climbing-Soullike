98d29090c22bf55268b5f938b48288662229c72f
Baek jaemin <110534217+hanamza@users.noreply.github.com>
2024-06-04 19:50:04 +0900

    status

--- Assets/Map/PlayerInteraction.cs
+++ Assets/Map/PlayerInteraction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab18cc9f448db1a70f579d5219780471444db664b3811d7b11d3795a26e0b122
-size 1167
+oid sha256:ebb90b440151eaa97007d382a3d8f76cb95cf797d738f34c9f65bb49c0d9d6ae
+size 1864
