ccff35a59b3aad4eac0139d6250f66febc3578eb
shrxm <102349239+shrxm@users.noreply.github.com>
2024-06-05 21:48:14 +0900

    boss_C

--- Assets/Map/InteractableObject.cs
+++ Assets/Map/InteractableObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:976e9d4d84b3a90e147ae5d3a61e3fb73a58cb79f086483c7b95794cba42fda5
-size 780
+oid sha256:05cd5c7381da68e21cd966745d99fe1da764e22118643614eb342bd5495917e1
+size 1910
