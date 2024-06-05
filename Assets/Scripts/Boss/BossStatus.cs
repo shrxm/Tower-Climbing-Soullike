840b69919e9127abf17abc6eeb96714d91a635e3
21LEEKANGHYUN <127746054+21LEEKANGHYUN@users.noreply.github.com>
2024-06-05 22:46:53 +0900

    BOSSUI
    BOSSUI 입니다.

--- Assets/Scripts/Boss/BossStatus.cs
+++ Assets/Scripts/Boss/BossStatus.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14f18e3c336e8f72f1c324e67571b62c6410dfb38e48128d6da0321b558a5cd8
-size 261
+oid sha256:759a14f8bafb877ae2b7e92d908065818adda4ee8bdbc689c43366de5b9ffafa
+size 1103
