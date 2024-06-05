6e17262e80aad3d7d5fe8b6e6ff07bb6502287bf
brawlkim <rudgks3374@gmail.com>
2024-06-05 19:28:17 +0900

    kh_22
    Die

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2545e5035130bc6bfef50f71382d19abb4f63f1e7218889b70ea39e08f8779d0
-size 9469
+oid sha256:6ae2f8353ce9dbd165cab104100c465a82c0dc62bbe38e60b436d141bdfa6985
+size 9441
