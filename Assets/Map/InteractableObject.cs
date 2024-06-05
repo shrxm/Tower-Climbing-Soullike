1d76d885979046a2e9a2bdb5b68cba7641896c45
brawlkim <rudgks3374@gmail.com>
2024-06-06 00:04:36 +0900

    kh_23
    12

--- Assets/Map/InteractableObject.cs
+++ Assets/Map/InteractableObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05cd5c7381da68e21cd966745d99fe1da764e22118643614eb342bd5495917e1
-size 1910
+oid sha256:c183b381fd71e0a108b20bfedcb07065c7fbf3be5480d0fc1dd8b266b5a1b799
+size 1823
