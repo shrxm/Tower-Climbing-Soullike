2e91b13df4e40a50aaf9da665b17ec6d7aae536d
shrxm <102349239+shrxm@users.noreply.github.com>
2024-06-07 01:41:16 +0900

    qwe

--- Assets/Map/PlayerInteraction.cs
+++ Assets/Map/PlayerInteraction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dd5a96dd065af5929c23dabe527486ba1b4633d1e905ea29cbcb33dbedeebc4
-size 1862
+oid sha256:782b29aeace38b7d37aa0ff76e762c264b59e2b6e29610c1ebfade83582d67a1
+size 1870
