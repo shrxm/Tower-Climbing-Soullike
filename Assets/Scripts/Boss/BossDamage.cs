8ad71a6b4cdbadaff1509b1708c8dcc7b5d831bf
shrxm <102349239+shrxm@users.noreply.github.com>
2024-06-07 16:28:28 +0900

    boss_balanse

--- Assets/Scripts/Boss/BossDamage.cs
+++ Assets/Scripts/Boss/BossDamage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7aa947d2207a9893fad49b8f54b1eba2eee76e827ec93b19c8be6c2b9c739b2
-size 2412
+oid sha256:6f80fa777152124ebb2716cb71ff841392974857270944cddc6592bcc3d9a33b
+size 2614
