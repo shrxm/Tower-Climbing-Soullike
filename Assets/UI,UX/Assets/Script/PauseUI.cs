d9c40b2a8983d76dae6a585ce759faeecd156ba5
21LEEKANGHYUN <127746054+21LEEKANGHYUN@users.noreply.github.com>
2024-06-07 23:47:19 +0900

    esc err modify
    메인메뉴갈때 활성화창들 비활성화 시킴으로써 오류 수정

--- Assets/UI,UX/Assets/Script/PauseUI.cs
+++ Assets/UI,UX/Assets/Script/PauseUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3aa99543070e18e9ac97c7ecbbfdd8bbef7e4472e45f2ee59ecf1016a006ec01
-size 2491
+oid sha256:fcbdd0283ef590f9cd414d1a6e24af733adc33adaaec8dd34cfa318bd230b771
+size 3054
